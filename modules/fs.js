bf9a0a3c36761510aa0be560723603966b7b1794
Matheus Henrique <matt.henrique13700@gmail.com>
2022-10-07 16:51:38 -0300

    first commit

---
+++ modules/fs.js
@@ -0,0 +1,52 @@
+const fs = require("fs");
+const path = require("path");
+
+// Criar uma pasta
+
+fs.mkdir(path.join(__dirname, "/test"), (error) => {
+  if (error) {
+    return console.log("Erro: ", error);
+  }
+  console.log("Pasta criada com sucesso!");
+});
+
+// Criar um arquivo
+
+fs.writeFile(
+  path.join(__dirname, "/test", "test.txt"),
+  "hello node!",
+  (error) => {
+    if (error) {
+      return console.log("Erro: ", error);
+    }
+
+    console.log("Pasta criada com sucesso!");
+
+    //Adicionar à um arquivo
+
+    fs.appendFile(
+      path.join(__dirname, "/test", "test.txt"),
+      "hello world",
+      (error) => {
+        if (error) {
+          return console.log("Erro: ", error);
+        }
+
+        console.log("Arquivo modificado com sucesso!");
+
+        //Ler um arquivo
+
+        fs.readFile(
+          path.join(__dirname, "/test", "test.txt"),
+          "utf-8",
+          (error, data) => {
+            if (error) {
+              return console.log("Erro: ", error);
+            }
+            console.log(data);
+          }
+        );
+      }
+    );
+  }
+);
