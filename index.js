9bce799d017d8d1d6ceb0bcead430620e97ebf8c
Your Name <matt.henrique13700@gmail.com>
2022-10-08 00:23:07 -0300

    start postman

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ connectToDatabase();
 // require("./modules/path")
 //require("./modules/fs");
 //require("./modules/http");
-//require("./modules/express");
+require("./modules/express");
 
 //const person = new Person("Matheus");
--- modules/express.js
+++ modules/express.js
@@ -1,7 +1,9 @@
 const express = require("express");
-//const UserModel = require("../src/models/user.model");
+const UserModel = require("../src/models/user.model");
 
 const app = express();
+
+app.use(express.json());
 
 app.get("/home", (req, res) => {
   // res.contentType("application/html");
@@ -25,15 +27,15 @@ app.get("/users", (req, res) => {
   res.status(200).json(users);
 });
 
-// app.post("/users", async (req, res) => {
-//   try {
-//     const user = await UserModel.creat(req.body);
+app.post("/users", async (req, res) => {
+  try {
+    const user = await UserModel.creat(req.body);
 
-//     res.status(201).send.json(user);
-//   } catch (error) {
-//     res.status(500).send(error.message);
-//   }
-// });
+    res.status(201).send.json(user);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
 
 const port = 8080;
 
